2dbc621d1e1cbe2efaca4b327387286ef95415a2
lonegamedev <official@lonegamedev.com>
2023-04-01 20:37:40 +0200

    initial godot4 version

--- camera_spinner.gd
+++ camera_spinner.gd
@@ -1,8 +1,8 @@
-extends Spatial
+extends Node3D
 
 var angle:float = 0.0
-export(float) var angular_speed:float = 0.1
+@export var angular_speed: float:float = 0.1
 
 func _process(delta):
 	angle += delta * angular_speed
-	transform.basis = Basis(Quat(Vector3.UP, angle))
+	transform.basis = Basis(Quaternion(Vector3.UP, angle))
--- grass.gd
+++ grass.gd
@@ -1,15 +1,39 @@
-tool
-extends MultiMeshInstance
+@tool
+extends MultiMeshInstance3D
 
 const MeshFactory = preload("mesh_factory.gd")
 const GrassFactory = preload("grass_factory.gd")
 
-export(Vector2) var blade_width:Vector2 = Vector2(0.01, 0.02) setget set_blade_width
-export(Vector2) var blade_height:Vector2 = Vector2(0.04, 0.08) setget set_blade_height
-export(Vector2) var sway_yaw:Vector2 = Vector2(0.0, 10.0) setget set_sway_yaw
-export(Vector2) var sway_pitch:Vector2 = Vector2(0.04, 0.08) setget set_sway_pitch
-export(Mesh) var mesh:Mesh = null setget set_mesh
-export(float) var density:float = 1.0 setget set_density
+@export var blade_width: Vector2 = Vector2(0.01, 0.02): 
+	set(new_value): 
+		blade_width = new_value
+		rebuild()
+
+@export var blade_height: Vector2 = Vector2(0.04, 0.08):
+	set(new_value):
+		blade_height = new_value
+		rebuild()
+		
+@export var sway_yaw: Vector2 = Vector2(0.0, 10.0):
+	set(new_value):
+		sway_yaw = new_value
+		rebuild()
+		
+@export var sway_pitch: Vector2 = Vector2(0.04, 0.08):
+	set(new_value):
+		sway_pitch = new_value
+		rebuild()
+	
+@export var mesh: Mesh = null:
+	set(new_value):
+		mesh = new_value
+		
+@export var density: float = 1.0:
+	set(new_value):
+		density = new_value
+		if density < 1.0:
+			density = 1.0
+		rebuild()
 
 func rebuild():
 	if !multimesh:
@@ -23,40 +47,14 @@ func rebuild():
 		sway_pitch,
 		sway_yaw
 	)
-	if spawns.empty():
+	if spawns.is_empty():
 		return
 	multimesh.mesh = MeshFactory.simple_grass()
 	multimesh.transform_format = MultiMesh.TRANSFORM_3D
-	multimesh.set_custom_data_format(MultiMesh.CUSTOM_DATA_FLOAT)
-	multimesh.set_color_format(MultiMesh.COLOR_NONE)
+	multimesh.use_custom_data = true
+	multimesh.use_colors = false
 	multimesh.instance_count = spawns.size()
 	for index in multimesh.instance_count:
 		var spawn:Array = spawns[index]
 		multimesh.set_instance_transform(index, spawn[0])
 		multimesh.set_instance_custom_data(index, spawn[1])
-
-func set_blade_width(p_blade_width:Vector2):
-	blade_width = p_blade_width
-	rebuild()
-	
-func set_blade_height(p_blade_height:Vector2):
-	blade_height = p_blade_height
-	rebuild()
-	
-func set_sway_yaw(p_sway_yaw:Vector2):
-	sway_yaw = p_sway_yaw
-	rebuild()
-
-func set_sway_pitch(p_sway_pitch:Vector2):
-	sway_pitch = p_sway_pitch
-	rebuild()
-	
-func set_mesh(p_mesh:Mesh):
-	mesh = p_mesh
-	rebuild()
-
-func set_density(p_density:float):
-	density = p_density
-	if density < 1.0:
-		density = 1.0
-	rebuild()
